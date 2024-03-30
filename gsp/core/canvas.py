08c88a0963b904f4c2269312333531c6f7d0c745
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2024-03-30 16:27:22 +0100

    Save actual size (not transform)

--- gsp/core/canvas.py
+++ gsp/core/canvas.py
@@ -35,19 +35,19 @@ class Canvas:
 
         from .. import transform
 
-        self._width = width
-        self._height = height
-        self._dpi = dpi
-
         if isinstance(width, transform.Transform):
             width = width.evaluate(variables = { "dpi": dpi })
         if isinstance(height, transform.Transform):
             height = height.evaluate(variables = { "dpi": dpi })
 
+        self._width = width
+        self._height = height
+        self._dpi = dpi
+
         self._figure = plt.figure(frameon=False, dpi=self._dpi)
         self._figure.patch.set_alpha(0.0)
-        self._figure.set_size_inches(width / self._dpi,
-                                     height /self._dpi)
+        self._figure.set_size_inches(self._width / self._dpi,
+                                     self._height /self._dpi)
 
         canvas = self._figure.canvas
         canvas.mpl_connect('resize_event', lambda event: self._figure.canvas.draw())
