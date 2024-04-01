4923ee51905612bc9c3096e60cb5ec69a1d26fda
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2024-04-01 08:18:03 +0200

    Code cleanup

--- examples/simple-points.py
+++ examples/simple-points.py
@@ -6,7 +6,6 @@
 import glm
 import camera
 import numpy as np
-import matplotlib.pyplot as plt
 from gsp import core, visual, transform
 
 canvas   = core.Canvas(512, 512, 100.0)
@@ -22,5 +21,6 @@ sizes = np.linspace(0.05, 12.0,n)**2
 black, white = core.Color(0,0,0,1), core.Color(1,1,1,1)
 points = visual.Points(P, sizes, black, black, 0)
 points.render(viewport, camera.model, camera.view, camera.proj)
-# plt.savefig("../docs/assets/simple-points.png")
+
+import matplotlib.pyplot as plt
 plt.show()
--- examples/simple-segments.py
+++ examples/simple-segments.py
@@ -3,13 +3,14 @@
 # License: BSD 3 clause
 """ Segments example """
 
+import glm
+import camera
 import numpy as np
-import matplotlib.pyplot as plt
-from gsp import core, visual, transform, glm
+from gsp import core, visual, transform
 
-canvas = core.Canvas(512, 512, 100.0)
-viewport = core.Viewport(canvas, 0, 0, 512, 512)
-camera = glm.Camera("ortho")
+canvas = core.Canvas()
+viewport = core.Viewport(canvas)
+camera = camera.Camera("ortho")
 
 n = 50
 P = glm.vec3(2*n).reshape(-1,2,3)
@@ -17,8 +18,9 @@ P[:] = (0.0, -0.5, 0.0), (0.0, +0.5, 0.0)
 P[:,0,0] = np.linspace(-0.75, +0.75, n)
 P[:,1,0] = P[:,0,0] + 0.1
 LW = np.linspace(0.1, 5.0, n)
-points = visual.Segments(P, line_widths=LW)
-points.render(viewport, camera.model, camera.view, camera.proj)
+segments = visual.Segments(P, line_widths=LW)
+segments.render(viewport, camera.model, camera.view, camera.proj)
+camera.connect(viewport, "motion",  segments.render)
 
-# plt.savefig("../docs/assets/simple-segments.png")
+import matplotlib.pyplot as plt
 plt.show()
